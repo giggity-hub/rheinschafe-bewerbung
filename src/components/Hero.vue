bbebd6f4ea0549f3cefd3cc39c3a4da6d19cf93d
giggity-hub <imigstefan@gmail.com>
2022-05-09 22:06:22 +0200

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="bg-black w-full h-screen flex-col flex relative lg:(flex-row-reverse justify-end)">
+    <div class="    lg:text-right flex-shrink-0">
+        <h1 class="hero-text">Moin Leude</h1>
+        <h2 class="hero-text">Stefan hier</h2>
+    </div>
+    <div class="bullshit relative h-0 flex-grow  lg:( flex-grow-0 h-full) flex items-end" >
+        <img class="max-h-full object-contain" alt="Hero-banner" src="../assets/hero-1080x1200.png" />
+    </div>
+</div>
+
+
+
+</template>
+
+<style>
+
+.hero-text{
+    @apply text-white text-8xl p-5;
+    /* font-size: */
+}
+
+
+/* .bullshit{
+    display: flex;
+    flex-grow: 1;
+    position: relative;
+    flex-shrink:1;
+} */
+
+</style>
---
+++ src/components/IchBin.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { reactive } from 'vue';
+
+
+const iAm = [
+    {
+        'icon': '🧠',
+        'title': 'Big Brain',
+        'text': 'Ich hab gerade nen 1.3er Schnitt und wurde für meine herrausragenden Leistungen auch mit einem Deutschland Stipedium belohnt'
+    }
+]
+
+let activeAccordion = reactive(-1)
+
+function toggle(index){
+    activeAccordion = (index === activeAccordion ? -1 : index)
+    console.log(index);
+}
+
+
+// export default{
+//           data(){
+//               return{
+//                   iAm,
+//                   activeToggle: -1,
+//               }
+//           }
+//       }
+
+</script>
+
+<template>
+
+<h1>Ich bin ein...</h1>
+
+
+<div v-for="(item, index) in iAm" class="bg-blue-500">
+    <div @click="toggle(index)">
+        {{item.title}}
+    </div>
+    <div :class="{'h-0': index != activeAccordion}" class="overflow-hidden">
+        {{item.text}}
+    </div>
+    
+</div>
+
+</template>
