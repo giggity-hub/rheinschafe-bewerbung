5b6384da460e8e6aced39c63ab44dc1c9eec576c
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2022-05-10 10:56:07 +0200

    more data + toggle functionality

--- src/components/IchBin.vue
+++ src/components/IchBin.vue
@@ -1,47 +1,65 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive,ref } from 'vue';
 
 
 const iAm = [
     {
+        'icon': '🎓',
+        'title': 'Stilechter Student',
+        'text': 'Ich Studiere seit dem xxx Angewandte Informatik an der Universität Duisburg Essen'
+    },
+    {
         'icon': '🧠',
         'title': 'Big Brain',
         'text': 'Ich hab gerade nen 1.3er Schnitt und wurde für meine herrausragenden Leistungen auch mit einem Deutschland Stipedium belohnt'
+    },
+    {
+        'icon': '🩳',
+        'title': 'Cargo Shorts Connoisseur',
+        'text': 'asdfasdfasdfasdfasdf'
+    },
+    {
+        'icon': '🎨',
+        'title': 'Gimp Gott',
+        'text': 'Das erstellen von zahlreichen Dank Memes hat mich die Wege der Bildbearbeitung mit GIMP und Inkscape gelehrt'
+    },
+    {
+        'icon': '',
+        'title': 'Javascript Junkie',
+        'text': 'asdf asdf asdf asdfasd;flk ja;sdlfkj a;sdlkfj;asldkfj a;sdlkfj a;sdlkfja;sd lkfjas;dlfkja; sdflkj '
     }
 ]
 
-let activeAccordion = reactive(-1)
+let activeAccordion = ref(-1)
 
 function toggle(index){
-    activeAccordion = (index === activeAccordion ? -1 : index)
-    console.log(index);
+    activeAccordion.value = (index === activeAccordion.value ? -1 : index)
+    console.log(activeAccordion);
 }
 
 
-// export default{
-//           data(){
-//               return{
-//                   iAm,
-//                   activeToggle: -1,
-//               }
-//           }
-//       }
+
 
 </script>
 
 <template>
+<section class="mx-auto w-full p-5 max-w-250">
 
 <h1>Ich bin ein...</h1>
 
-
+{{activeAccordion}}
 <div v-for="(item, index) in iAm" class="bg-blue-500">
-    <div @click="toggle(index)">
-        {{item.title}}
+    <div @click="toggle(index)" class="bg-red-500 p-5 text-xl rounded-xl">
+        {{item.icon}}{{item.title}}
     </div>
-    <div :class="{'h-0': index != activeAccordion}" class="overflow-hidden">
+    <div :class="{'h-0': index != activeAccordion}" class="overflow-hidden text-lg p-5">
         {{item.text}}
     </div>
     
 </div>
-
+</section>
 </template>
+
+<style>
+    
+</style>
