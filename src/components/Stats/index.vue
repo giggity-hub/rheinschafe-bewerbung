0365f14c0ae46eb87bc4c04df2e6761b106bf939
giggity-hub <imigstefan@gmail.com>
2022-05-22 16:03:20 +0200

    added stats section

--- src/components/Stats/index.vue
+++ src/components/Stats/index.vue
@@ -1,3 +1,20 @@
 <template>
-
+    <div class="flex flex-col md:flex-row">
+        <div class="bg-green-500 w-full">
+            sdfgsdfgs
+        </div>
+        <div class="bg-orange-400 w-full character-model">
+            sdfgsdfg
+        </div>
+    </div>
 </template>
+
+<style>
+.character-model{
+    background-color: purple;
+    background-image:  linear-gradient(#444cf7 1px, transparent 1px), linear-gradient(to right, #444cf7 1px, #e5e5f7 1px);
+    background-size: 20px 20px;
+}
+
+
+</style>
