6f47742c060d97b8cddce03a9cc2932e2d96e974
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2022-05-24 15:25:42 +0200

    added ratingstar component

--- src/components/Stats/index.vue
+++ src/components/Stats/index.vue
@@ -1,10 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+import RatingStars from '../RatingStars.vue';
+
+const stats = [
+    {
+        title: 'Intelligence',
+        rating: 5,
+        description: 'sooos saaaaaaaaas schooosch'
+    },
+    {
+        title: 'Strength',
+        rating: 10,
+        description: 'sheeeeeeeeeeeeeeeesh shush'
+    }
+]
+const showRating = ref([false, false]);
+function fuck(index){
+    console.log('sheesh');
+    showRating.value[index]=true
+}
+
+</script>
+
 <template>
     <div class="flex flex-col md:flex-row">
-        <div class="bg-green-500 w-full">
-            <svg>
-                
-            </svg>
-            sdfgsdfgs
+        <div class="bg-green-500 w-full p-5">
+            <div class="">
+                <div v-for="(stat, index) in stats" class="mb-5 bg-orange-400" @mouseenter="fuck(index)">
+                    <div class="flex justify-between">
+                        <div>{{stat.title}}</div>
+                        <RatingStars :rating="stat.rating" :show="showRating[index]"/>
+                        
+                    </div>
+                    <div>
+                        {{stat.description}}
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="bg-orange-400 w-full character-model">
             sdfgsdfg
